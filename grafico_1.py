ad93f7a53c587ab0f0ea7c1d6dd08d587d07da8f
Meus2Bits <57447289+Meus2Bits@users.noreply.github.com>
2019-11-15 15:50:10 -0300

    Create grafico_1.py

---
+++ grafico_1.py
@@ -0,0 +1,13 @@
+# %matplotlib inline
+
+# Importando as bibliotecas
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Criando os dados
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+# Representando graficamente os pontos
+plt.plot(x, y)
+plt.show()
