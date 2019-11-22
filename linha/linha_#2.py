8c052a5eeec6765abf13f2ebee087b3876123b0e
Meus2Bits <meus2bits@gmail.com>
2019-11-22 11:41:11 -0100

    Update linha_#2.py

--- linha/linha_#2.py
+++ linha/linha_#2.py
@@ -1,17 +1,25 @@
+# CONFIGURANDO
+# -------------------------------------------
 #%matplotlib inline
 
 import matplotlib as mpl
-%mpl.rcParams['figure.dpi'] = 100
+#%mpl.rcParams['figure.dpi'] = 100
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+# CRIANDO DADOS
+# -------------------------------------------
 x = np.linspace(0, 10, 100)
 y = np.sin(x)
 
+# TÍTULOS E LEGENDAS
+# -------------------------------------------
 plt.title('Função de Onda')
 plt.ylabel('f(x)')
 plt.xlabel('x')
 
+# REPRESENTANDO GRAFICAMENTE
+# -------------------------------------------
 plt.plot(x, y)
 plt.show()
